706eda1a31e5e25072e727b680cb79b57bd14436
mackenzie k <mackenziekeeley@gmail.com>
2024-04-12 22:39:11 +0000

    i did it!

--- lab8/script.py
+++ lab8/script.py
@@ -1,4 +1,4 @@
-jq -r '.[] | "\(.name),\(.html_url),\(.updated_at),\(.visibility)"' data/schacon.repos.json \
+jq -r '.[] | "\(.name),\(.html_url),\(.updated_at),\(.visibility)"' /workspace/json-practice/data/schacon.repos.json \
 | head -n 5 \
 | sed '1i name,html_url,updated_at,visibility' > chacon.csv
 
--- lab8/script.sh
+++ lab8/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -s "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" \
-| jq -r '.features[].properties.receiptTime' \
-| head -n 6
+cat ../data/simple.single.json | jq 'limit(6; .[]).receiptTime'
+
